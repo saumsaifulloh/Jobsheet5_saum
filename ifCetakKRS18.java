dad6540e267e2840f5195541f97736f02fdab4a7
saumsaifulloh <saummm18.com>
2025-10-01 14:01:31 +0700

    percobaan 1

---
+++ ifCetakKRS18.java
@@ -0,0 +1,20 @@
+//PERCOBAAN 1
+// MUHAMMAD SAUM SAIFULLOH /1E/18/SISTEM INFORMASI BISNIS
+
+import java.util.Scanner;
+
+public class ifCetakKRS18{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (ya/tidak): ");
+        boolean uktLunas = input.nextBoolean();
+
+        if (uktLunas) {
+            System.out.print("Pembayaran UKT terverifikasi?");
+            System.out.print("Silakan cetak KRS dan minta tanda tangan DPA");
+
+        }
+    }
+}
